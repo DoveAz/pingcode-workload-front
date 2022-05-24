902339acf45c9166601e23c043885dbe03994056
DoveAz <lijianmin1993@gmail.com>
2022-05-24 15:36:27 +0800

    ✨  增加折线图

--- src/pages/chart/index.tsx
+++ src/pages/chart/index.tsx
@@ -1,7 +1,7 @@
 import { useSetState } from 'ahooks'
 import ReactECharts from 'echarts-for-react'
 import { map } from 'lodash'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { service } from '../../plugins/service'
 export default function Chart() {
@@ -29,43 +29,12 @@ export default function Chart() {
     xAxis: {
       type: 'category',
       boundaryGap: false,
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      data: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'],
     },
     yAxis: {
       type: 'value',
     },
-    series: [
-      {
-        name: 'Email',
-        type: 'line',
-        stack: 'Total',
-        data: [120, 132, 101, 134, 90, 230, 210],
-      },
-      {
-        name: 'Union Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [220, 182, 191, 234, 290, 330, 310],
-      },
-      {
-        name: 'Video Ads',
-        type: 'line',
-        stack: 'Total',
-        data: [150, 232, 201, 154, 190, 330, 410],
-      },
-      {
-        name: 'Direct',
-        type: 'line',
-        stack: 'Total',
-        data: [320, 332, 301, 334, 390, 330, 320],
-      },
-      {
-        name: 'Search Engine',
-        type: 'line',
-        stack: 'Total',
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-      },
-    ],
+    series: [],
   })
   useEffect(function () {
     service.post('workload/chart').then((res) => {

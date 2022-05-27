ed8e7a42c28845e2f10922dd39cd9125368d7e01
DoveAz <lijianmin1993@gmail.com>
2022-05-27 11:58:14 +0800

    ✨ 折线图增加上周

--- src/pages/chart/index.tsx
+++ src/pages/chart/index.tsx
@@ -5,9 +5,41 @@ import { useEffect } from 'react'
 
 import { service } from '../../plugins/service'
 export default function Chart() {
-  const [option, setOption] = useSetState({
+  const [option1, setOption1] = useSetState({
     title: {
-      text: '工时折线图(每周)',
+      text: '工时折线图(本周)',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+      },
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [],
+  })
+
+  const [option2, setOption2] = useSetState({
+    title: {
+      text: '工时折线图(上周)',
     },
     tooltip: {
       trigger: 'axis',
@@ -38,11 +70,23 @@ export default function Chart() {
   })
   useEffect(function () {
     service.post('workload/chart').then((res) => {
-      setOption({
+      setOption1({
         legend: {
-          data: map(res.data, 'user.displayName'),
+          data: map(res.data.current, 'user.displayName'),
         },
-        series: res.data.map((item: any) => {
+        series: res.data.current.map((item: any) => {
+          return {
+            name: item.user.displayName,
+            type: 'line',
+            data: item.result,
+          }
+        }),
+      })
+      setOption2({
+        legend: {
+          data: map(res.data.prev, 'user.displayName'),
+        },
+        series: res.data.prev.map((item: any) => {
           return {
             name: item.user.displayName,
             type: 'line',
@@ -53,8 +97,9 @@ export default function Chart() {
     })
   }, [])
   return (
-    <div className="p-[15px] bg-white">
-      <ReactECharts option={option} notMerge lazyUpdate theme={'theme_name'} />
+    <div className="p-[15px]">
+      <ReactECharts option={option1} notMerge lazyUpdate theme={'theme_name'} className="bg-white p-[15px]" />
+      <ReactECharts option={option2} notMerge lazyUpdate theme={'theme_name'} className="bg-white p-[15px] mt-[15px]" />
     </div>
   )
 }
